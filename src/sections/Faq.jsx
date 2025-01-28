3174320a446249492d8d4d95efc80337a8be1f4c
hisham rehab <hishamrehab309@gmail.com>
2025-01-28 20:21:13 +0200

    finish testimonal section

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -7,17 +7,20 @@ const Faq = () => {
   return (
     <section>
       <Element name="faq" className="relative">
-        <div className="relative z-2 py-28">
+        <div className="container relative z-2 py-28">
           <div>
-            <h3 className="h3 max-md:h5 max-w-640 max-lg:max-md mb-7 text-p4">              Curiosity didn't kill the cat, it gave it answers.
+            <h3 className="h3 max-md:h5 max-w-640 max-lg:max-w-md mb-7 text-p4">
+              Curiosity didn't kill the cat, it gave it answers.
             </h3>
-            <p className="body-1 max-lg:max-w-sm "> You've got questions, we've got answers.</p>
+            <p className="body-1 max-lg:max-w-sm">
+              You've got questions, we've got answers.
+            </p>
           </div>
-          <div className="faq-line_after w-0.5 h-full absolute left-[calc(50%-0.5px)] top-0 -z-1 bg-s2" />
+
+          <div className="faq-line_after w-0.5 h-full absolute left-[calc(50%-1px)] top-0 -z-1 bg-s2" />
         </div>
 
         <div className="faq-glow_before relative z-2 border-2 border-s2 bg-s1">
-
           <div className="container flex gap-10 max-lg:block">
             <div className="rounded-half absolute -top-10 left-[calc(50%-40px)] z-4 flex size-20 items-center justify-center border-2 border-s2 bg-s1">
               <img src="/images/faq-logo.svg" alt="logo" className="size-1/2" />
@@ -29,17 +32,14 @@ const Faq = () => {
               ))}
             </div>
 
-
-
             <div className="relative flex-1 lg:pt-24">
               {faq.slice(halfLength).map((item, index) => (
                 <FaqItem key={item.id} item={item} index={halfLength + index} />
               ))}
             </div>
-
-
           </div>
-          <div className="faq-line_after absolute left-[calc(50%-1px)] top-0 -z-1 h-full w-0.5 bg-s2 max-lg:hidden" />
+
+          <div className="faq-lin_after absolute left-[calc(50%-1px)] top-0 -z-1 h-full w-0.5 bg-s2 max-lg:hidden" />
         </div>
       </Element>
     </section>
